01d5f0546e74b3219bc8860f76e80cd2e953f463
josdem <joseluis.delacruz@gmail.com>
2018-04-30 18:29:19 -0500

    Adding assert all examples

---
+++ src/main/java/com/jos/dem/junit/Person.java
@@ -0,0 +1,21 @@
+package com.jos.dem.junit;
+
+public class Person {
+
+  private String nickname;
+  private String email;
+
+  public Person(String nickname, String email){
+    this.nickname = nickname;
+    this.email = email;
+  }
+
+  public String getNickname(){
+    return this.nickname;
+  }
+
+  public String getEmail(){
+    return this.email;
+  }
+
+}
--- src/test/java/com/jos/dem/junit/LambdaAssertionTest.java
+++ src/test/java/com/jos/dem/junit/LambdaAssertionTest.java
@@ -1,6 +1,8 @@
 package com.jos.dem.junit;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.stream.Stream;
 
@@ -9,12 +11,24 @@ import org.junit.jupiter.api.Test;
 
 public class LambdaAssertionTest {
 
+  private Person person = new Person("josdem", "joseluis.delacruz@gmail.com");
+
   @Test
   @DisplayName("Should show how we can use lambdas in a test")
-  void shouldTestLambdaExpression() {
+  public void shouldTestLambdaExpression() {
     assertTrue(Stream.of(1, 2, 3)      
       .mapToInt(Integer::intValue)
       .sum() == 6, () -> "Sum should be 6");
   }
+
+  @Test
+  @DisplayName("Should assert all person attributes at once")
+  void shouldAssertAllPersonAttributes() {
+      assertAll("person",
+          () -> assertEquals("josdem", person.getNickname()),
+          () -> assertEquals("joseluis.delacruz@gmail.com", person.getEmail())
+      );
+  }
+
   
 }
