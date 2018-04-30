b6e91887d16bee74fd32a9aa04adda26f13b3340
josdem <joseluis.delacruz@gmail.com>
2018-04-30 18:19:36 -0500

    Adding display name attribute

--- src/test/java/com/jos/dem/junit/LambdaAssertionTest.java
+++ src/test/java/com/jos/dem/junit/LambdaAssertionTest.java
@@ -4,11 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.stream.Stream;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LambdaAssertionTest {
 
   @Test
+  @DisplayName("Should show how we can use lambdas in a test")
   void shouldTestLambdaExpression() {
     assertTrue(Stream.of(1, 2, 3)      
       .mapToInt(Integer::intValue)
