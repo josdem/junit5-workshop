69411444a05b230b42c0016ccd8ab1364d0990ed
josdem <joseluis.delacruz@gmail.com>
2018-05-02 22:39:42 -0500

    Adding assumptions example

---
+++ src/test/java/com/jos/dem/junit/AssumptionShowTest.java
@@ -0,0 +1,32 @@
+package com.jos.dem.junit;
+
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import java.util.logging.Logger;
+
+class AssumptionShowTest {
+
+  private final Logger log = Logger.getLogger(AssumptionShowTest.class.getName());
+
+  private Person person = new Person("josdem", "joseluis.delacruz@gmail.com");
+
+  @Test
+  @DisplayName("Assume that is a Gmail account")
+  void shouldKnowIfIsGmailAccount(){
+    assumeTrue(person.getEmail().endsWith("gmail.com"));
+    log.info("Test continues ...");
+    assertEquals("josdem", person.getNickname());
+  }
+
+   @Test
+   @DisplayName("Assuming something based in conditions")
+   void testAssumingThat() {
+     assumingThat(2 > 1, () -> System.out.println("This should happen!"));
+     assumingThat(2 < 1, () -> System.out.println("This should never happen!"));
+   }
+
+}
