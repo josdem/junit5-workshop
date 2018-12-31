d33983da2b9f153d5ca485208a2e8f26670116d7
josdem <joseluis.delacruz@gmail.com>
2018-12-31 17:15:35 -0500

    Adding enabledOnOs test example

--- src/test/java/com/jos/dem/junit/ConditionsShowTest.java
+++ src/test/java/com/jos/dem/junit/ConditionsShowTest.java
@@ -1,10 +1,13 @@
 package com.jos.dem.junit;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.condition.OS.MAC;
+import static org.junit.jupiter.api.condition.OS.LINUX;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.condition.EnabledIf;
+import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 
 import java.util.logging.Logger;
@@ -26,6 +29,13 @@ class ConditionsShowTest {
   @EnabledIf("(java.time.LocalDate).now().getDayOfWeek() == 'MONDAY'")
   void shouldRunIfMonday() {
     log.info("Running: Conditions if is Monday");
+    assertTrue(true);
+  }
+
+  @Test
+  @EnabledOnOs({ LINUX, MAC })
+  void shouldRunOnLinuxOrMac() {
+    log.info("Running: Conditions if Linux or Mac");
     assertTrue(true);
   }
 
