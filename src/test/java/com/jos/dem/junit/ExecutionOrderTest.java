e9276fda23d39a445f7e1c0fa1bbab19035cacb4
josdem <joseluis.delacruz@gmail.com>
2020-08-22 18:01:30 -0400

    Adding execution order example

---
+++ src/test/java/com/jos/dem/junit/ExecutionOrderTest.java
@@ -0,0 +1,33 @@
+package com.jos.dem.junit;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+
+import java.util.logging.Logger;
+
+@TestMethodOrder(OrderAnnotation.class)
+public class ExecutionOrderTest {
+
+  private final Logger log = Logger.getLogger(ExecutionOrderTest.class.getName());
+
+  @Test
+  @Order(1)
+  @DisplayName("first test")
+  void shouldExecuteFirst(TestInfo testInfo) {
+    log.info(String.format("Executing %s ...", testInfo.getDisplayName()));
+  }
+
+  @Test
+  @Order(2)
+  @DisplayName("second test")
+  void shouldExecuteSecond(TestInfo testInfo) {
+    log.info(String.format("Executing %s ...", testInfo.getDisplayName()));
+  }
+
+  @Test
+  @Order(3)
+  @DisplayName("third test")
+  void shouldExecuteThird(TestInfo testInfo) {
+    log.info(String.format("Executing %s ...", testInfo.getDisplayName()));
+  }
+}
