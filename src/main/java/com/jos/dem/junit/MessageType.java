c1d696978e7934e5948080f1c03199bb649064d9
josdem <joseluis.delacruz@gmail.com>
2020-09-20 17:41:48 -0400

    Adding method source example

---
+++ src/main/java/com/jos/dem/junit/MessageType.java
@@ -0,0 +1,5 @@
+package com.jos.dem.junit;
+
+public enum MessageType {
+    SMS, PUSH_NOTIFICATION
+}
--- src/test/java/com/jos/dem/junit/ParameterizedShowTest.java
+++ src/test/java/com/jos/dem/junit/ParameterizedShowTest.java
@@ -2,12 +2,15 @@ package com.jos.dem.junit;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.EnumSet;
 import java.util.logging.Logger;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -43,10 +46,23 @@ class ParameterizedShowTest {
 
   @DisplayName("Allow csv files as parameters")
   @ParameterizedTest
-  @CsvFileSource(resources = "/csv.txt", numLinesToSkip = 1)
+  @CsvFileSource(resources = "/csv/persons.txt", numLinesToSkip = 1)
   void shouldAllowCsvFileSource(int id, String nickname, String email) {
     assertNotNull(id);
     assertTrue(nickname.length() > 3);
     assertTrue(email.endsWith("email.com"));
   }
+
+  @DisplayName("Allow json files as arguments")
+  @ParameterizedTest
+  @MethodSource("messages")
+  void shouldBeValidMessages(String json){
+    log.info("json: " + json);
+  }
+
+  private static Stream<Arguments> messages() {
+    return Stream.of(
+            Arguments.of("/json/sms.json"), Arguments.of("/json/pushNotification.json"));
+  }
+
 }
